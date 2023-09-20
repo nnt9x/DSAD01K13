84d4d9310684bbedc1aa654678446d6a39e7c652
NNT9x <42613109+nnt9x@users.noreply.github.com>
2023-09-20 17:04:17 +0700

    update

--- src/main/java/linkedlist2/DoublyLinkedList.java
+++ src/main/java/linkedlist2/DoublyLinkedList.java
@@ -41,6 +41,38 @@ public class DoublyLinkedList {
         size++;
     }
 
+    public void add(int data, int position) {
+        if (position == 0) {
+            addFirst(data);
+            return;
+        }
+        if (position == size) {
+            addLast(data);
+            return;
+        }
+        // Thêm vào giua -> cần tim position và position
+        if (position > 0 && position < size) {
+            // Chèn vào giữa ??
+            // Làm thế nào để tìm tìm dc position -1 ??
+            Node previousPosition = head;
+            for (int i = 0; i < (position - 1); i++) {
+                previousPosition = previousPosition.getNext();
+            }
+            // node postion
+            Node currentNode = previousPosition.getNext();
+
+            Node tmp = new Node(data);
+
+            previousPosition.setNext(tmp);
+            tmp.setPrevious(previousPosition);
+
+            tmp.setNext(currentNode);
+            currentNode.setPrevious(tmp);
+
+            size ++;
+        }
+    }
+
     public void display() {
         if (size == 0) {
             System.out.println("DoublyLinkedList is empty!");
--- src/main/java/linkedlist2/Main.java
+++ src/main/java/linkedlist2/Main.java
@@ -7,6 +7,14 @@ public class Main {
         linkedList.addFirst(20);
         linkedList.display();
         linkedList.addLast(30);
+        linkedList.display(); // 20 10 30
+
+        linkedList.add(0, 0);
         linkedList.display();
+        linkedList.add(40, 4);
+        linkedList.display(); // 0 20 10 30 40
+        linkedList.add(50, 3); // 0 20 10 50 30 40
+        linkedList.display();
+
     }
 }
